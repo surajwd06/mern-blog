c853b5159bdd92dd00a2bddb29cbd095826fd9a3
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-03-18 12:47:48 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignUp from './pages/SignUp'
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
-}
+};
 
-export default App
+export default App;
