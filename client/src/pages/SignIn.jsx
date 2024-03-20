f59277a2febc133392d9a13b4dc779105ecb121c
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-03-20 11:19:54 +0530

    create a signup page ui

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import React from "react";
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+<div>Sign in</div>
+  );
+};
 
-export default SignIn
+export default SignIn;
