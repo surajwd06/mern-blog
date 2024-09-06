5337775ae7752e89cfc82807842390c23943fb37
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-09-06 11:00:17 +0530

    add update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,54 @@
 
+import User from "../model/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 
-export const test=(req,res)=>{
-    res.json({ message: "Api is working" });
-}
+export const test = (req, res) => {
+  res.json({ message: "Api is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "password must be at 6 characters"));
+    }
+    req.body.password = await bcryptjs.hash(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 6 || req.body.username.length > 20) {
+      return next(errorHandler(400, "username must be 7 to 20 characters"));
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "username cannot include space"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "username cannot contain letters and numbers")
+      );
+    }
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            profilePicture: req.body.profilePicture,
+            password: req.body.password,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updateUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import useRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose
@@ -17,9 +18,10 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 app.listen(4000, () => {
-  console.log("Server is running is 3000 !");
+  console.log("Server is running is 4000 !");
 });
 
 
